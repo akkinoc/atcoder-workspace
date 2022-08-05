8d867bbfc733987a6575c19c4741993b9625f145
akkinoc <akkinoc@gmail.com>
2022-08-06 01:22:24 +0900

    [AC] typical90 typical90_a

--- atcoder.jp/typical90/typical90_a/Main.kt
+++ atcoder.jp/typical90/typical90_a/Main.kt
@@ -3,22 +3,23 @@ fun main() {
     val k = readLine()!!.toInt()
     val a = readLine()!!.split(" ").map { it.toInt() }
     val s = (1 until l).search { s ->
-        val c = mutableListOf<Int>()
-        a.forEach {
-            val e = c.lastOrNull() ?: 0
-            if (it - e >= s && l - it >= s) c += it
+        var c = 0
+        var e = 0
+        for (it in a) if (it - e >= s && l - it >= s) {
+            c++
+            e = it
         }
-        c.size >= k
+        c >= k
     }
     print(s)
 }
 
 fun IntRange.search(b: (Int) -> Boolean): Int {
-    var f = this.first - 1
-    var l = this.last + 1
-    while (l - f > 1) {
-        val m = f + (l - f) / 2
-        if (b(m)) f = m else l = m
+    var l = this.first - 1
+    var r = this.last + 1
+    while (r - l > 1) {
+        val m = l + (r - l) / 2
+        if (b(m)) l = m else r = m
     }
-    return f
+    return l
 }
