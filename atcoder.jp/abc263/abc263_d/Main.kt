e597306fc313e0e76edc9fa08ef2ed170617e232
Akihiro Kondo <akkinoc@gmail.com>
2022-08-07 18:05:31 +0900

    Remove archive (atcoder.jp/abc263/abc263_d) for test auto archive

--- atcoder.jp/abc263/abc263_d/Main.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    val (n, l, r) = readLine()!!.split(" ").map { it.toInt() }
-    val a = readLine()!!.split(" ").map { it.toInt() }
-    val sl = LongArray(n + 1)
-    for (i in 1..n) sl[i] = minOf(l.toLong() * i, sl[i - 1] + a[i - 1])
-    val sr = LongArray(n + 1)
-    for (i in n - 1 downTo 0) sr[i] = minOf(r.toLong() * (n - i), a[i] + sr[i + 1])
-    print((0..n).map { i -> sl[i] + sr[i] }.min())
-}
