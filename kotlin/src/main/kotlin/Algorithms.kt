e74ed80e06a7b674d25b99624ae523bf1744b7c0
Akihiro Kondo <akkinoc@gmail.com>
2022-08-11 20:49:00 +0900

    Add Algorithms.lcm function

--- kotlin/src/main/kotlin/Algorithms.kt
+++ kotlin/src/main/kotlin/Algorithms.kt
@@ -1,11 +1,9 @@
 object Algorithms {
 
-    tailrec fun gcd(a: Int, b: Int): Int {
-        return if (b == 0) a else gcd(b, a % b)
-    }
+    tailrec fun gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+    tailrec fun gcd(a: Long, b: Long): Long = if (b == 0L) a else gcd(b, a % b)
 
-    tailrec fun gcd(a: Long, b: Long): Long {
-        return if (b == 0L) a else gcd(b, a % b)
-    }
+    fun lcm(a: Int, b: Int): Int = if (a == 0 || b == 0) 0 else a / gcd(a, b) * b
+    fun lcm(a: Long, b: Long): Long = if (a == 0L || b == 0L) 0L else a / gcd(a, b) * b
 
 }
