4e90601c6f09e25fd95df05050b4869cd2d35d53
Akihiro Kondo <akkinoc@gmail.com>
2022-08-13 20:13:41 +0900

    Update MainTest

--- kotlin/src/test/kotlin/MainTest.kt
+++ kotlin/src/test/kotlin/MainTest.kt
@@ -11,7 +11,9 @@ class MainTest {
 
     @Test
     fun `test - main`() {
-        invoke("test").shouldBe("test")
+        invoke("case1").shouldBe("case1")
+        invoke("case2").shouldBe("case2")
+        invoke("case3").shouldBe("case3")
     }
 
 }
