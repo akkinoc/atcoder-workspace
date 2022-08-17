9e7672f748ce3b5a1167646794777f585d425007
Akihiro Kondo <akkinoc@gmail.com>
2022-08-17 22:23:11 +0900

    Improve ListAlgo performance

--- kotlin/src/main/kotlin/dev/akkinoc/atcoder/workspace/kotlin/algo/ListAlgo.kt
+++ kotlin/src/main/kotlin/dev/akkinoc/atcoder/workspace/kotlin/algo/ListAlgo.kt
@@ -13,9 +13,19 @@ object ListAlgo {
      * @param r The number of elements to select.
      * @return A sequence of permutations.
      */
-    fun <T> List<T>.perm(r: Int = size): Sequence<List<T>> = sequence {
-        if (r < 1) yield(emptyList())
-        else forEachIndexed { i, e -> (take(i) + drop(i + 1)).perm(r - 1).forEach { yield(listOf(e) + it) } }
+    fun <T> List<T>.perm(r: Int): Sequence<List<T>> {
+        val i = IntArray(size) { it }
+        val c = IntArray(r) { size - it }
+        return generateSequence(List(r) { get(i[it]) }) s@{ _ ->
+            val p = (r - 1 downTo 0).find f@{ p ->
+                if (--c[p] > 0) return@f true
+                i[size - 1] = i[p].also { i.copyInto(i, p, p + 1) }
+                c[p] = size - p
+                false
+            } ?: return@s null
+            i[size - c[p]] = i[p].also { i[p] = i[size - c[p]] }
+            List(r) { get(i[it]) }
+        }
     }
 
     /**
@@ -26,9 +36,14 @@ object ListAlgo {
      * @param r The number of elements to select.
      * @return A sequence of repeated permutations.
      */
-    fun <T> List<T>.rperm(r: Int = size): Sequence<List<T>> = sequence {
-        if (r < 1) yield(emptyList())
-        else forEach { e -> rperm(r - 1).forEach { yield(listOf(e) + it) } }
+    fun <T> List<T>.rperm(r: Int): Sequence<List<T>> {
+        val i = IntArray(r)
+        return generateSequence(i.map(::get)) s@{ _ ->
+            val p = (r - 1 downTo 0).find { i[it] < size - 1 } ?: return@s null
+            ++i[p]
+            i.fill(0, p + 1)
+            i.map(::get)
+        }
     }
 
     /**
@@ -39,9 +54,14 @@ object ListAlgo {
      * @param r The number of elements to select.
      * @return A sequence of combinations.
      */
-    fun <T> List<T>.comb(r: Int = size): Sequence<List<T>> = sequence {
-        if (r < 1) yield(emptyList())
-        else forEachIndexed { i, e -> drop(i + 1).comb(r - 1).forEach { yield(listOf(e) + it) } }
+    fun <T> List<T>.comb(r: Int): Sequence<List<T>> {
+        val i = IntArray(r) { it }
+        return generateSequence(i.map(::get)) s@{ _ ->
+            var p = (r - 1 downTo 0).find { i[it] < size - r + it } ?: return@s null
+            var n = i[p]
+            while (p < r) i[p++] = ++n
+            i.map(::get)
+        }
     }
 
     /**
@@ -52,9 +72,13 @@ object ListAlgo {
      * @param r The number of elements to select.
      * @return A sequence of repeated combinations.
      */
-    fun <T> List<T>.rcomb(r: Int = size): Sequence<List<T>> = sequence {
-        if (r < 1) yield(emptyList())
-        else forEachIndexed { i, e -> drop(i).rcomb(r - 1).forEach { yield(listOf(e) + it) } }
+    fun <T> List<T>.rcomb(r: Int): Sequence<List<T>> {
+        val i = IntArray(r)
+        return generateSequence(i.map(::get)) s@{ _ ->
+            val p = (r - 1 downTo 0).find { i[it] < size - 1 } ?: return@s null
+            i.fill(i[p] + 1, p)
+            i.map(::get)
+        }
     }
 
 }
--- kotlin/src/test/kotlin/dev/akkinoc/atcoder/workspace/kotlin/algo/ListAlgoTest.kt
+++ kotlin/src/test/kotlin/dev/akkinoc/atcoder/workspace/kotlin/algo/ListAlgoTest.kt
@@ -5,6 +5,7 @@ import dev.akkinoc.atcoder.workspace.kotlin.algo.ListAlgo.perm
 import dev.akkinoc.atcoder.workspace.kotlin.algo.ListAlgo.rcomb
 import dev.akkinoc.atcoder.workspace.kotlin.algo.ListAlgo.rperm
 import io.kotest.matchers.sequences.shouldContainExactly
+import io.kotest.matchers.sequences.shouldHaveSize
 import org.junit.jupiter.api.Test
 
 /**
@@ -14,7 +15,7 @@ class ListAlgoTest {
 
     @Test
     fun `test - perm`() {
-        listOf('A', 'B', 'C').perm().shouldContainExactly(
+        listOf('A', 'B', 'C').perm(3).shouldContainExactly(
             listOf('A', 'B', 'C'), listOf('A', 'C', 'B'),
             listOf('B', 'A', 'C'), listOf('B', 'C', 'A'),
             listOf('C', 'A', 'B'), listOf('C', 'B', 'A')
@@ -26,12 +27,13 @@ class ListAlgoTest {
         )
         listOf('A', 'B', 'C').perm(1).shouldContainExactly(listOf('A'), listOf('B'), listOf('C'))
         listOf('A', 'B', 'C').perm(0).shouldContainExactly(emptyList())
-        emptyList<Any>().perm().shouldContainExactly(emptyList())
+        emptyList<Any>().perm(0).shouldContainExactly(emptyList())
+        List(100) { it }.perm(4).shouldHaveSize(94_109_400)
     }
 
     @Test
     fun `test - rperm`() {
-        listOf('A', 'B', 'C').rperm().shouldContainExactly(
+        listOf('A', 'B', 'C').rperm(3).shouldContainExactly(
             listOf('A', 'A', 'A'), listOf('A', 'A', 'B'), listOf('A', 'A', 'C'),
             listOf('A', 'B', 'A'), listOf('A', 'B', 'B'), listOf('A', 'B', 'C'),
             listOf('A', 'C', 'A'), listOf('A', 'C', 'B'), listOf('A', 'C', 'C'),
@@ -49,21 +51,23 @@ class ListAlgoTest {
         )
         listOf('A', 'B', 'C').rperm(1).shouldContainExactly(listOf('A'), listOf('B'), listOf('C'))
         listOf('A', 'B', 'C').rperm(0).shouldContainExactly(emptyList())
-        emptyList<Any>().rperm().shouldContainExactly(emptyList())
+        emptyList<Any>().rperm(0).shouldContainExactly(emptyList())
+        List(100) { it }.rperm(4).shouldHaveSize(100_000_000)
     }
 
     @Test
     fun `test - comb`() {
-        listOf('A', 'B', 'C').comb().shouldContainExactly(listOf('A', 'B', 'C'))
+        listOf('A', 'B', 'C').comb(3).shouldContainExactly(listOf('A', 'B', 'C'))
         listOf('A', 'B', 'C').comb(2).shouldContainExactly(listOf('A', 'B'), listOf('A', 'C'), listOf('B', 'C'))
         listOf('A', 'B', 'C').comb(1).shouldContainExactly(listOf('A'), listOf('B'), listOf('C'))
         listOf('A', 'B', 'C').comb(0).shouldContainExactly(emptyList())
-        emptyList<Any>().comb().shouldContainExactly(emptyList())
+        emptyList<Any>().comb(0).shouldContainExactly(emptyList())
+        List(100) { it }.comb(5).shouldHaveSize(75_287_520)
     }
 
     @Test
     fun `test - rcomb`() {
-        listOf('A', 'B', 'C').rcomb().shouldContainExactly(
+        listOf('A', 'B', 'C').rcomb(3).shouldContainExactly(
             listOf('A', 'A', 'A'), listOf('A', 'A', 'B'), listOf('A', 'A', 'C'),
             listOf('A', 'B', 'B'), listOf('A', 'B', 'C'), listOf('A', 'C', 'C'),
             listOf('B', 'B', 'B'), listOf('B', 'B', 'C'), listOf('B', 'C', 'C'),
@@ -75,7 +79,8 @@ class ListAlgoTest {
         )
         listOf('A', 'B', 'C').rcomb(1).shouldContainExactly(listOf('A'), listOf('B'), listOf('C'))
         listOf('A', 'B', 'C').rcomb(0).shouldContainExactly(emptyList())
-        emptyList<Any>().rcomb().shouldContainExactly(emptyList())
+        emptyList<Any>().rcomb(0).shouldContainExactly(emptyList())
+        List(100) { it }.rcomb(5).shouldHaveSize(91_962_520)
     }
 
 }
