002479f319d7f3f55a9cbf600bfca610ff2ba35d
Akihiro Kondo <akkinoc@gmail.com>
2022-09-02 14:22:17 +0900

    Shorten parameter names

--- kotlin/src/main/kotlin/dev/akkinoc/atcoder/workspace/kotlin/algo/IntRangeAlgo.kt
+++ kotlin/src/main/kotlin/dev/akkinoc/atcoder/workspace/kotlin/algo/IntRangeAlgo.kt
@@ -9,13 +9,13 @@ object IntRangeAlgo {
      * Finds an element by binary search algorithm.
      *
      * @receiver A number range.
-     * @param fn The function that determines whether an element is selected.
+     * @param f The function that determines whether an element is selected.
      * @return The first element selected, or `null` if not found.
      */
-    fun IntRange.bsearch(fn: (e: Int) -> Boolean): Int? {
+    fun IntRange.bsearch(f: (e: Int) -> Boolean): Int? {
         var l = first - 1
         var r = last + 1
-        while (l < r - 1) (l + (r - l ushr 1)).also { if (fn(it)) r = it else l = it }
+        while (l < r - 1) (l + (r - l ushr 1)).also { if (f(it)) r = it else l = it }
         return r.takeIf { it <= last }
     }
 
--- kotlin/src/main/kotlin/dev/akkinoc/atcoder/workspace/kotlin/algo/LongRangeAlgo.kt
+++ kotlin/src/main/kotlin/dev/akkinoc/atcoder/workspace/kotlin/algo/LongRangeAlgo.kt
@@ -9,13 +9,13 @@ object LongRangeAlgo {
      * Finds an element by binary search algorithm.
      *
      * @receiver A number range.
-     * @param fn The function that determines whether an element is selected.
+     * @param f The function that determines whether an element is selected.
      * @return The first element selected, or `null` if not found.
      */
-    fun LongRange.bsearch(fn: (e: Long) -> Boolean): Long? {
+    fun LongRange.bsearch(f: (e: Long) -> Boolean): Long? {
         var l = first - 1L
         var r = last + 1L
-        while (l < r - 1) (l + (r - l ushr 1)).also { if (fn(it)) r = it else l = it }
+        while (l < r - 1) (l + (r - l ushr 1)).also { if (f(it)) r = it else l = it }
         return r.takeIf { it <= last }
     }
 
